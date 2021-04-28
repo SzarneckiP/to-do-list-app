3c23ab74ce79c0b376a5c88107151c735e97d3e4
SzarneckiP <siarniuk@gmail.com>
2021-04-28 18:36:50 +0200

    add task and remove task

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,43 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 class App extends React.Component {
 
+  state = {
+    tasks: [],
+    taskName: '',
+  };
+
+  componentDidMount() {
+    this.socket = io('localhost:8000', { transports: ['websocket', 'polling', 'flashsocket'] });
+    this.socket.on('removeTask', delTask => {
+      this.removeTask(delTask);
+    });
+    this.socket.on('addTask', task => {
+      this.addTask(task);
+    });
+  };
+
+  removeTask = (delTask) => {
+    this.setState({
+      tasks: this.state.tasks.filter(task => task.id !== delTask)
+    });
+    this.socket.emit('removeTask', delTask)
+  };
+
+  submitForm = (event) => {
+    event.preventDefault();
+    const taskName = { name: this.state.taskName };
+    this.addTask(taskName);
+    this.socket.emit('addTask', taskName);
+  };
+
+  addTask = (task) => {
+    this.setState({ tasks: [...this.state.tasks, task] })
+  };
+
   render() {
+    const { taskName } = this.state;
     return (
       <div className="App">
 
@@ -14,13 +49,18 @@ class App extends React.Component {
           <h2>Tasks</h2>
 
           <ul className="tasks-section__list" id="tasks-list">
-            <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-            <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+            {this.state.tasks.map(task => (
+              <li key={task.id}>
+                {task.name}
+                <button className="btn btn--red" onClick={() => this.removeTask(task.id)}>Remove</button>
+              </li>
+            ))}
           </ul>
 
           <form id="add-task-form">
-            <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-            <button className="btn" type="submit">Add</button>
+            <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name"
+              value={taskName} onChange={(event) => this.setState({ taskName: event.target.value })} />
+            <button className="btn" type="submit" onClick={e => this.submitForm(e)}>Add</button>
           </form>
 
         </section>
--- server.js
+++ server.js
@@ -2,20 +2,33 @@ const express = require('express');
 const path = require('path');
 const socket = require('socket.io');
 
+let tasks = [];
+
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/client')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/client/public/index.html'));
+    res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
-
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running...')
 });
 
 const io = socket(server);
+
+io.on('connection', (socket) => {
+    socket.emit('updateData', tasks);
+    socket.on('addTask', task => {
+        tasks.push(task);
+        socket.broadcast.emit('addTask', task);
+    });
+    socket.on('removeTask', delTask => {
+        tasks.filter(task => task.id !== delTask);
+        socket.broadcast.emit('removeTask', delTask);
+    });
+});
 
 app.use((req, res) => {
     res.status(404).send({ message: '404 not found...' })
