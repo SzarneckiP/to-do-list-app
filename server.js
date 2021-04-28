12a125f8600cb4478432dff8da5f66c37703a077
SzarneckiP <siarniuk@gmail.com>
2021-04-28 13:51:03 +0200

    Init project

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const socket = require('socket.io');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '/client')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
+
+
+const server = app.listen(process.env.PORT || 8000, () => {
+    console.log('Server is running...')
+});
+
+const io = socket(server);
+
+app.use((req, res) => {
+    res.status(404).send({ message: '404 not found...' })
+});
